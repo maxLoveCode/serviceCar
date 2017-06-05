beba05f425adbbaba3013934bea85a4cdb4d3ef9
maxLoveCode <maxkidy@gmail.com>
2017-06-05 16:05:03 +0800

    hd

--- serviceCar/src/main/java/serviceCar/controller/IndexController.java
+++ serviceCar/src/main/java/serviceCar/controller/IndexController.java
@@ -13,11 +13,15 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import pojo.User;
+import serviceCar.service.UserService;
 
 @Controller
 @RequestMapping("/")
 public class IndexController extends BaseController {
 
+	@Autowired
+	UserService userService;
 
 	/**
 	 * 跳转至api文档
@@ -41,7 +45,47 @@ public class IndexController extends BaseController {
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
 		return "login";
-	} 
+	}
+
+	/**
+	 * 登录操作
+	 * 
+	 * @param username
+	 * @param password
+	 * @param model
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String loginPost(String username, String password, Model model, HttpSession session) {
+		if (StringUtils.isEmpty(username)) {
+			model.addAttribute("error", "用户名不能为空");
+			return "login";
+		}
+		model.addAttribute("username", username);
+		if (StringUtils.isEmpty(password)) {
+			model.addAttribute("error", "密码不能为空");
+			return "login";
+		}
+		User admin = userService.findUserByUsername(username);
+		if (StringUtils.isEmpty(admin)) {
+			model.addAttribute("error", "用户名不存在");
+			return "login";
+		}
+		if (!admin.getPassword().equals(password)) {
+			model.addAttribute("error", "用户名或密码错误");
+			return "login";
+		}
+		
+		//更新登录时间
+		admin.setLogintime(new Date());
+		userService.updateByPrimaryKey(admin);
+
+		session.setAttribute(SESSION_ID, session.getId());
+		session.setAttribute(SESSION_NAME, admin);
+		session.setAttribute(WEBSOCKET_USERNAME, String.valueOf(admin.getId()));
+		return "index";
+	}
 
 	/**
 	 * 登出页面
@@ -63,11 +107,6 @@ public class IndexController extends BaseController {
 	 */
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String index() {
-		return "index";
-	}
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(){
 		return "index";
 	}
 }
